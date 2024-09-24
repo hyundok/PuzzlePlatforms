de1a4aa73a7a441a609ad57e54081de52ca09996
hyundok <ekfcjs1304@naver.com>
2024-09-24 15:43:00 +0900

    0924
    총 모델링 임포트

--- Source/PuzzlePlatforms/PuzzlePlatforms.Build.cs
+++ Source/PuzzlePlatforms/PuzzlePlatforms.Build.cs
@@ -8,6 +8,6 @@ public class PuzzlePlatforms : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG", "OnlineSubsystem", "OnlineSubsystemSteam" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "UMG", "OnlineSubsystem", "OnlineSubsystemSteam", "Niagara", "AIModule", "NavigationSystem" , "GameplayTasks"  });
 	}
 }
